91a205336af77c0d2218e8bd68d1e83a97f06cc4
katiecho <katiecho@localhost>
2024-10-30 14:19:49 -0500

    Commit

--- src/studio7/Die.java
+++ src/studio7/Die.java
@@ -1,10 +1,17 @@
 package studio7;
 
 public class Die {
+	
+	private int sides;
+	private int round; 
+	
+			
+	public Die (int sides, int round); 
+	int round = (int)(Math.random()*(100)/100);
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
